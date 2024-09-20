3e95786970b1e9f0b97df6350209a2ec718f432d
J. Ballesteros <joaquin.ballesteros@mdh.se>
2024-09-20 12:17:34 +0200

    Actualizacion lista enlazada

--- src/MemoriaDinamica/6ListaEnlazada.c
+++ src/MemoriaDinamica/6ListaEnlazada.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
 
 typedef struct Node * ptrnodo;
 
@@ -8,24 +10,30 @@ struct Node{
     ptrnodo next;
 };
 
-void insertar (struct Node ** copiaHead, int dato){
+void insertar (struct Node ** direccionHead, int dato){
     struct Node * nuevo = (struct Node *) malloc(sizeof(struct Node));
     if (nuevo == NULL){
         printf("No hay memoria disponible\n");
         exit(-1);
     }
     nuevo->data = dato;
-    nuevo->next = *copiaHead;
-    (*copiaHead) = nuevo;
-
-    
+    nuevo->next = (*direccionHead);
+    (*direccionHead)=nuevo;
+   
 }
 
+void destruir(struct Node ** direccionHead){
+   ;
+}
 
-void mostrar(struct Node * copiaHead){
-    while(copiaHead!=NULL){
-        printf("Dato: %d\nPosición de memoria:%p\n",copiaHead->data, copiaHead->next);
-        copiaHead = copiaHead->next;
+bool eliminar(struct Node ** direccionHead, int data){
+    ;
+}
+
+void mostrar(struct Node * direccionHead){
+    while(direccionHead!=NULL){
+        printf("Dato: %d\n Siguiente:%p\n",direccionHead->data, direccionHead->next);
+        direccionHead = direccionHead->next;
     }
 }
 
@@ -38,8 +46,21 @@ int main(){
     printf("\nEstructura antes de insertar:\n");
     mostrar(head);
     insertar(&head, 10);
-    printf("\nEstructura tras insertar:\n");
-    mostrar(head);
+    insertar(&head, 30);
+    insertar(&head, 20);
+    /*eliminar(&head,1);
+    eliminar(&head,30);
+    eliminar(&head,10);
+    eliminar(&head,20);
+
+    //VOlemos a insertar y destruimos
+    insertar(&head, 10);
+    insertar(&head, 30);
+    insertar(&head, 20);
+
+    destruir(&head);
+*/
+
     printf("\n");
 
     free(head->next);
